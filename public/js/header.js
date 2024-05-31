e88a64261f5833f5c8acb55076da54c19ac5ac59
Matthieu Jacques <Matthieu.Jacques.2077@gmail.com>
2024-05-31 16:24:41 +0200

    added help, improved players

--- public/js/header.js
+++ public/js/header.js
@@ -1,10 +1,25 @@
 const togglePlayersButton = document.getElementById('toggle-players');
 togglePlayersButton.addEventListener('click', function () {
     var playersDiv = document.getElementById('players');
-    var computedStyle = window.getComputedStyle(playersDiv);
-    if (computedStyle.display === 'none') {
-        playersDiv.style.display = 'flex';
+    var helpDiv = document.getElementById('help');
+    
+    if (playersDiv.style.right === '-20em' || playersDiv.style.right === '') {
+        playersDiv.style.right = '0';
+        helpDiv.style.right = '-20em';
     } else {
-        playersDiv.style.display = 'none';
+        playersDiv.style.right = '-20em';
+    }
+});
+
+const toggleHelpButton = document.getElementById('toggle-help');
+toggleHelpButton.addEventListener('click', function () {
+    var helpDiv = document.getElementById('help');
+    var playersDiv = document.getElementById('players');
+    
+    if (helpDiv.style.right === '-20em' || helpDiv.style.right === '') {
+        helpDiv.style.right = '0';
+        playersDiv.style.right = '-20em';
+    } else {
+        helpDiv.style.right = '-20em';
     }
 });
--- public/js/players.js
+++ public/js/players.js
@@ -3,24 +3,24 @@ export const displayPlayers = (me, players, fn) => {
   playerList.setAttribute('id', 'players-ul');
 
   players.forEach((player) => {
-    if (player.username === me) return;
+      if (player.username === me) return;
 
-    const listItem = document.createElement("li");
-    listItem.classList.add("player-item");
+      const listItem = document.createElement("li");
+      listItem.classList.add("player-item");
 
-    const playerName = document.createElement("span");
-    playerName.textContent = player.username;
+      const playerName = document.createElement("span");
+      playerName.textContent = player.username;
 
-    const button = document.createElement("button");
-    button.textContent = "Invite to play";
-    button.classList.add("invite-button");
-    button.onclick = function () {
-      fn({ from: me, to: player.username });
-    };
+      const button = document.createElement("button");
+      button.textContent = "Invite to play";
+      button.classList.add("invite-button");
+      button.onclick = function () {
+          fn({ from: me, to: player.username });
+      };
 
-    listItem.appendChild(playerName);
-    listItem.appendChild(button);
-    playerList.appendChild(listItem);
+      listItem.appendChild(playerName);
+      listItem.appendChild(button);
+      playerList.appendChild(listItem);
   });
 
   const currentUser = document.createElement("div");
@@ -33,13 +33,13 @@ export const displayPlayers = (me, players, fn) => {
   const currentPlayerContainer = document.getElementById("current-player");
 
   if (oldList) {
-    listContainer.removeChild(oldList);
+      listContainer.removeChild(oldList);
   }
 
   listContainer.appendChild(playerList);
 
   if (currentPlayerContainer.firstChild) {
-    currentPlayerContainer.removeChild(currentPlayerContainer.firstChild);
+      currentPlayerContainer.removeChild(currentPlayerContainer.firstChild);
   }
 
   currentPlayerContainer.appendChild(currentUser);
